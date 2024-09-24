c904e31b3e38dbf9ddc8562cb8f040bbde4e6b3a
AADIL-YP <154458532+AADIL-YP@users.noreply.github.com>
2024-09-24 21:23:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    alert(`Message sent!\nName: ${name}\nEmail: ${email}\nMessage: ${message}`);
+});
